a7f787c3860c7209d554ff34b5039461c8b7b61f
Sonia364 <c0872364@mylambton.ca>
2022-11-04 18:26:38 -0400

    Initial Commit

---
+++ Sonia_C0872364_LabTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sonia_C0872364_LabTest
+//
+//  Created by MacStudent on 2022-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
