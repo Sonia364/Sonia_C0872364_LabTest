4c78cf68b248b5d1f162e7efcd363a092cbe1c58
Sonia364 <c0872364@mylambton.ca>
2022-11-04 20:14:17 -0400

    Part 2(Timer) - added tab control

---
+++ Sonia_C0872364_LabTest/timerVC.swift
@@ -0,0 +1,32 @@
+//
+//  timerVC.swift
+//  Sonia_C0872364_LabTest
+//
+//  Created by MacStudent on 2022-11-04.
+//
+
+import UIKit
+
+class timerVC: UIViewController {
+
+    @IBOutlet weak var cancelBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
